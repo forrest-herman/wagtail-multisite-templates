86fa5b6774cc7bb2b3fc7d8af17d8470d11503ac
Forrest Herman <58410716+forrest-herman@users.noreply.github.com>
2021-09-23 01:55:01 -0400

    Update production.py

--- core/settings/production.py
+++ core/settings/production.py
@@ -30,10 +30,6 @@ ALLOWED_HOSTS = ["*"]
 # DEBUG = False
 DEBUG = True
 
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-MEDIA_URL = "/media/"
-
 try:
     from .local import *
 except ImportError:
